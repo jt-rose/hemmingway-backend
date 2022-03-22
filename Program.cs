6645e84deae59cf1f81115e177132292b30cef54
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-21 21:52:17 -0400

    connect graphql server

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
-app.MapControllers();
+// app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
 
 app.Run();
