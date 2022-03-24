4a868bc639b2a6faf1ad30affcd845741a3b26f9
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-24 12:43:04 -0400

    set up exercise factory and gql relationship

--- database/factories/ExerciseFactory.php
+++ database/factories/ExerciseFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Exercise>
@@ -17,7 +18,13 @@ class ExerciseFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'calories' => $this->faker->randomNumber(),
+            'date_of_exercise' => $this->faker->date(),
+            'minutes' => $this->faker->randomNumber(),
+            'user_id' => function() {
+                return User::factory()->create()->id;
+            }
         ];
     }
 }
