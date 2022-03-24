063c28131db94b0817c6fb6dc160f6e9320fede4
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-24 15:33:42 -0400

    set up user login and password hashing

---
+++ app/GraphQL/Mutations/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class Login
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $user = User::where('email', $args['email'])->first();
+
+        if (! $user || 
+        // $args['password'] != $user->password
+        ! Hash::check($args['password'], $user->password)
+        ) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.']
+            ]);
+        }
+
+        return $user->createToken($args['device'])->plainTextToken;
+    }
+}
