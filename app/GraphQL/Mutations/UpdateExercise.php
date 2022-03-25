7cd74ff3d0bc9f9fee2213114309cc6a93702059
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-25 14:03:04 -0400

    add exercise update resolver

---
+++ app/GraphQL/Mutations/UpdateExercise.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+use Illuminate\Validation\ValidationException;
+use App\Models\Exercise;
+
+class UpdateExercise
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $myExercise = Exercise::where(['id' => $args['id'], 'user_id' => $args['input']['user_id']])->first();
+        if (! $myExercise ) {
+            throw ValidationException::withMessages([
+                'user_id' => ['No matching record for this user']
+            ]);
+        }
+        $myExercise->name = $args['input']['name'];
+        $myExercise->minutes = $args['input']['minutes'];
+        $myExercise->calories = $args['input']['calories'];
+        $myExercise->date_of_exercise = $args['input']['date_of_exercise'];
+
+        $myExercise->save();
+
+        return $myExercise;
+    }
+}
