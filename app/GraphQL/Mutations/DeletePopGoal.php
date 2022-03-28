cfbbb8570582bf9ad9f3e1ed91138fac25e0f307
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-27 21:48:28 -0400

    add custom resolver for pop goal

---
+++ app/GraphQL/Mutations/DeletePopGoal.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\DailyPopGoal;
+use Illuminate\Validation\ValidationException;
+
+class DeletePopGoal
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $myPopGoal = PopGoal::where(['id' => $args['id'], 'user_id' => $args['user_id']])->delete();
+        if (! $myPopGoal ) {
+            throw ValidationException::withMessages([
+                'user_id' => ['No matching record for this user']
+            ]);
+        }
+        return true;
+    }
+}
